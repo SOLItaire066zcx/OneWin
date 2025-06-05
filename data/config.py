d76c3472a1df1197fbab302f575cba8d3f482a24
SOLItaire066zcx <smithkingsn691@gmail.com>
2025-06-06 00:08:38 +0200

    Update config.py

--- data/config.py
+++ data/config.py
@@ -16,7 +16,7 @@ PROXY_TYPE = "socks5" # http/socks5
 WORKDIR = "sessions/"
 
 # использование прокси
-USE_PROXY = True # True/False
+USE_PROXY = false # True/False
 
 # до какого уровня прокачивать
 UPGRADE_LEVEL = 20
